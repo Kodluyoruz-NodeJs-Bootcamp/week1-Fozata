55da13bd700da541bd63f012c92ad2d477e07196
Fozata <furkan@statetax.io>
2022-01-03 18:51:39 +0300

    week1

---
+++ script.js
@@ -0,0 +1,29 @@
+let dragged;
+
+document.addEventListener(
+  "dragstart",
+  function (event) {
+    dragged = event.target;
+  },
+  false
+);
+
+document.addEventListener(
+  "dragover",
+  function (event) {
+    event.preventDefault();
+  },
+  false
+);
+
+document.addEventListener(
+  "drop",
+  function (event) {
+    event.preventDefault();
+
+    if (event.target.className == "dropzone") {
+      event.target.appendChild(dragged);
+    }
+  },
+  false
+);
